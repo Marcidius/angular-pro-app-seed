8a4195166f5ff0bc0beb153519c3f8bb0c93bc42
ryan-king <ryan.king@beckshybrids.com>
2017-10-27 14:19:18 -0400

    Create meals component internals and create meal component itself with dummy html for the time being.  Add route to add a new meal.

---
+++ src/health/meals/containers/meal/meal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meal',
+  styleUrls: ['./meal.component.scss'],
+  template: `
+    <div class="meal">
+      I am a meal!
+    </div>
+  `
+})
+
+export class MealComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -6,10 +6,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 import { MealsComponent } from './containers/meals/meals.component';
+import { MealComponent } from './containers/meal/meal.component';
+
 
 // containers
 export const ROUTES: Routes = [
-  { path: '', component: MealsComponent }
+  { path: '', component: MealsComponent },
+  { path: 'new', component: MealComponent }
 ]
 
 @NgModule({
@@ -21,7 +24,8 @@ export const ROUTES: Routes = [
   ],
   exports: [],
   declarations: [
-    MealsComponent
+    MealsComponent,
+    MealComponent
   ]
 })
 export class MealsModule { }
